f4789a81c815ea96dc053f34b1f3c83791dafb43
Hend Ramadan <Hend.Ramadan@ai.menofia.edu.eg>
2025-12-03 19:49:55 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -9,7 +9,7 @@ import pandas as pd
 import os
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFaceEndpoint
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_classic.chains import RetrievalQA
 from langchain_core.prompts import PromptTemplate
 from langchain_community.llms import HuggingFaceHub
